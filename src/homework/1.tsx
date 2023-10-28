99a30f828c6e88eddb0abb3771a5d15f6371ce2d
Pavlo Lysiuk <121764119+PavloLysiuk@users.noreply.github.com>
2023-10-29 02:07:29 +0300

    Done

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,20 +1,30 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, ReactNode } from 'react';
+
+interface ObserverProps {
+  children: ReactNode;
+  onContentEndVisible: () => void;
+}
 
 // Опишіть Props
-export function Observer({ children, onContentEndVisible }: Props) {
+export function Observer({ children, onContentEndVisible }: ObserverProps) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
   const endContentRef = useRef(null);
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
+    interface Options {
+      rootMargin: string;
+      threshold: number;
+      root: null;
+    }
+    const options: Options = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
     };
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
         if (entry.intersectionRatio > 0) {
           onContentEndVisible();
           observer.disconnect();
